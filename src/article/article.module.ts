df66e13a32de50e1648b44296a524fba838450b0
Daniela Berger <daniela.berger@open.de>
2025-08-27 15:35:41 +0200

    removes unneeded files

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
-import { articleProviders } from './entities/article.providers';
-import { DatabaseModule } from '../database/database.module';
 import { Article } from './entities/article.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 
